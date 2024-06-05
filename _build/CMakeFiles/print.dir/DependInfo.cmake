0d54051e468338272a13201e90744c4abe11da03
pupok <pupok@Ubuntu22.04.4.myguest.virtualbox.org>
2024-06-05 23:51:54 +0200

    1

---
+++ _build/CMakeFiles/print.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/oleg/1andronyx/workspace/projects/lab03/lab02/sources/print.cpp" "CMakeFiles/print.dir/lab02/sources/print.cpp.o" "gcc" "CMakeFiles/print.dir/lab02/sources/print.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
